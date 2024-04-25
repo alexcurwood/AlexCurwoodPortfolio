6e14fac2fc1c15e5d2405be83733c7c707ba319c
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-04-25 21:02:06 +0100

    Basic layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,10 @@
+import "./page.css";
+
 export default function Home() {
-  return <p>test</p>;
+  return (
+    <div className="Header">
+      <h2>Alex Curwood</h2>
+      <h1>Aspiring Junior Software Developer</h1>
+    </div>
+  );
 }
